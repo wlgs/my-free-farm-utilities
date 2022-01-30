48507cfec84f071abd7d7bc501a8b1e495df4ca4
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-01-30 22:06:45 +0100

    added ids + function descriptions

---
+++ data/readme.md
@@ -0,0 +1,12 @@
+# Plants' ids
+
+| Plant name  | Id of the plant |
+| ------------- | ------------- |
+| `wheat`  | 1 |
+| `corn`  | 2 |
+| `carrot`  | 17  |
+| `radish`  | 19  |
+| `strawberry`  | 20  |
+| `tomato`  | 21  |
+| `onion`  | 22  |
+| `spinach`  | 23  |
--- src/useful.js
+++ src/useful.js
@@ -1,22 +1,24 @@
-function plantAll(){
+// Plants all over field with given plant id
+function plantAll(id){
     for(let i =1; i<=120; i++)
         cache.push({
             feld: i,
             felder: [i],
             kategorie: undefined,
             mode: 0,
-            pflanze: 19,
+            pflanze: id,
             produkt: 0})
 }
 
-function waterAll(){
+// Waters all plants (matching id) on the field
+function waterAll(id){
     for(let i =1; i<=120; i++)
         cache.push({feld: i,
         felder: [i],
         kategorie: "v",
         mode: 2,
         pflanze: null,
-        produkt: "19"})
+        produkt: "id"})
 }
 
 
